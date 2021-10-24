e850af96569676d74e3abd17cdf3aa270b2797d8
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2021-10-24 16:04:36 +0100

    added authentication and extra features

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+// Required Imports
+import { ByMoralis } from "react-moralis";
+import { Box, Stack, Heading, Center } from "@chakra-ui/react";
+import { WatchAddress } from "./Forms/WatchAddress";
+import { Auth } from "./Auth/Auth";
 
 function App() {
+  // Set Up Authentication
   return (
     <div className="App">
-      <h1>Whale Alert App🐋🚨</h1>
+      <Center>
+        <Box w={500} mt={5}>
+          <Center>
+            <Heading size="lg">Whale Alert App🐋🚨</Heading>
+          </Center>
+          <Stack spacing={4}>
+            <Auth/>
+            <Box>
+              <WatchAddress />
+            </Box>
+          </Stack>
+          <Box float="right" mt={4}>
+            <ByMoralis width={200} variant="dark" />
+          </Box>
+        </Box>
+      </Center>
     </div>
   );
 }
