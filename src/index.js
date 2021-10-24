299665a3e46538686b79340fc3c0e62a95bdad04
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2021-10-24 16:05:06 +0100

    slight formatting

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { ChakraProvider } from '@chakra-ui/react';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { MoralisProvider } from "react-moralis";
 
 const theme = extendTheme({
-  config:{
-    initialColorMode: 'dark'
-  }
-})
+  config: {
+    initialColorMode: "dark",
+  },
+});
 
-const appId = "G7ICEnoBmI47SQxy9mZMYbekQxEUgkFjfK8Po8jZ"
-const serverUrl = "https://rhehashcrz5z.usemoralis.com:2053/server"
-
+const appId = "G7ICEnoBmI47SQxy9mZMYbekQxEUgkFjfK8Po8jZ";
+const serverUrl = "https://rhehashcrz5z.usemoralis.com:2053/server";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -23,5 +22,5 @@ ReactDOM.render(
       </MoralisProvider>
     </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
